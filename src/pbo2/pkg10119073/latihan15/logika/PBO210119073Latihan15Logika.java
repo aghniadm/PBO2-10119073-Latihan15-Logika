ad9d1f89c2689cbc13bea712366f83dbf364a2b8
aghniadm <aghniadewii@gmail.com>
2020-10-19 09:05:27 +0700

    PBO2-10119073-Latihan15-Logika

---
+++ src/pbo2/pkg10119073/latihan15/logika/PBO210119073Latihan15Logika.java
@@ -0,0 +1,24 @@
+package pbo2.pkg10119073.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * logika
+ *
+ */
+public class PBO210119073Latihan15Logika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
